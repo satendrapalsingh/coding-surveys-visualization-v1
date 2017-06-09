9ba1baa035b164edc5b5f3fe3b523775828fb9c0
satenndrra <satenndrra@gmail.com>
2017-06-09 10:00:42 +0000

    code to extract and print csv columns on page

--- scripts/csv-parser.js
+++ scripts/csv-parser.js
@@ -1,1 +1,11 @@
 // code to parse the d3.csv-array-converted csv file //
+    alert("hello there");
+    
+/*    
+    var csvData;
+    
+    d3.csv("survey-data/2016-FCC-New-Coders-Survey-Data.csv", function(error, dataset){
+        alert("about to load csvData");
+        csvData = dataset;
+    });
+*/    
