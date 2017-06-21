799ad6b2dfea7457c721c2c7e42b6fc0fa913348
satenndrra <satenndrra@gmail.com>
2017-06-21 11:47:35 +0000

    added comments to code

--- scripts/csv-parser.js
+++ scripts/csv-parser.js
@@ -4,14 +4,17 @@
     var csvColumns;         // var for storing csv column names
     var popElement = "";    // var for holding and passing generated html button elements
 
+// debug function, remove definition and calls once done
     function printButton(columnIndex){
         console.log("button index " + " " + columnIndex);  
     }               
 
+// load and parse the included csv data file, with d3
     d3.csv("survey-data/2016-FCC-New-Coders-Survey-Data.csv", function(error, dataset){
         csvData = dataset;
         csvColumns = Object.keys(csvData[0]);
-        
+
+// generate the html for displaying csv columns as buttons, include column index         
         for (var idx in csvColumns){
             popElement += "<button id='" 
                         + csvColumns[idx] 
@@ -24,7 +27,7 @@
                         + "</button>" 
                         + "<br>";
         };
-        
+// push generated button html into dom        
         document.getElementById("columnIndex").innerHTML = popElement;
     
     });
