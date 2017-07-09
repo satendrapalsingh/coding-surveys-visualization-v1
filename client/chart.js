fd4357aceecbc7e5b2589e43d18473aec866dd20
satenndrra <satenndrra@gmail.com>
2017-07-09 06:13:46 +0000

    debugging the make pie chart function

--- client/chart.js
+++ client/chart.js
@@ -37,13 +37,52 @@
 
 // create a pie chart
     function makePieChart(inData, inColumn){
-        var width = 300;
-        var height = 300;
+        var width = 960;
+        var height = 500;
         var radius = Math.min(width, height)/2;
-        
-        var color = d3.scaleOrdinal()
-	                .range(["#2C93E8","#838690","#F56C4E"]);
 
+    var data = [10, 20, 50];    
+
+    var color = d3.scaleOrdinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888"]);
+    
+    var arc = d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+    
+    var labelArc = d3.arc()
+        .outerRadius(radius - 40)
+        .innerRadius(radius - 40);
+    
+    var pie = d3.pie()
+        .sort(null)
+        .value(function(d) { return d; });
+    
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    
+     var g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+    
+      g.append("path")
+          .attr("d", arc)
+          .style("fill", function(d) { return color(d.data); });
+    
+      g.append("text")
+          .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .text(function(d) { return d.data; });
+
+        
+/*
+        var color = d3.scaleOrdinal()
+        	.range(["#2C93E8","#838690","#F56C4E"]);
+        
         var pie = d3.pie()
                 .value(function(d){return d.Count})(inData);
 
@@ -76,6 +115,7 @@
             .attr("transform", function(d) {return "translate(" + labelArc.centroid(d) + ")";})
             .text(function(d) {return d.Age;})
             .style("fill", "#fff");
+*/
     }
         
         
